9b6575059bd6f072505e1f8e3e8a8eba81b6c7fa
ARedondoVilla <alexredondovilla@gmail.com>
2020-12-28 19:38:24 +0000

    Error relationship with Tweets table

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -9,11 +11,14 @@ class Users(db.Model):
     first_name = db.Column(db.String(60))
     last_name = db.Column(db.String(120))
     password = db.Column(db.String(80))
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean())
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    tweet = db.relationship("Tweets")
 
+    def __str__(self):
+        # return '<@%r>' % self.username
+        return f'@{self.username}'
+        
     def serialize(self):
         return {
             "id": self.id,
@@ -21,5 +26,25 @@ class Users(db.Model):
             "username": self.username,
             "first_name": self.first_name,
             "last_name": self.last_name
+            # do not serialize the password, its a security breach
+        }
+
+
+class Tweets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    text = db.Column(db.String(400))
+
+    user = db.relationship("Users")
+    
+    def __str__(self):
+        # return '<User %r>' % self.username
+        return f'@{self.username}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "text": self.text
             # do not serialize the password, its a security breach
         }
