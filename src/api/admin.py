2341f321570dbec3445683718360d7f0815c35a6
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-02 19:23:10 +0000

    Solved relationships errors ==> psql command

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, Users, Tweets
+from .models import db, Users, Tweets, Comments, Likes, Follows
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,6 +13,9 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))
     admin.add_view(ModelView(Tweets, db.session))
+    admin.add_view(ModelView(Comments, db.session))
+    admin.add_view(ModelView(Likes, db.session))
+    admin.add_view(ModelView(Follows, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -18,7 +18,8 @@ class Users(db.Model):
 
     def __str__(self):
         # return '<@%r>' % self.username
-        return f'@{self.username}'
+        return f'@{self.username} ole ole'
+        # Este texto aparece cuando hay que insertar dentro de una tabla el id de un usuario en otra tabla
         
     def serialize(self):
         return {
@@ -41,7 +42,7 @@ class Tweets(db.Model):
     
     def __str__(self):
         # return '<User %r>' % self.username
-        return f'@{self.user.username}'
+        return f'{self.text} de @{self.user.username}'
 
     def serialize(self):
         return {
@@ -50,3 +51,55 @@ class Tweets(db.Model):
             "text": self.text
             # do not serialize the password, its a security breach
         }
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    tweet_id = db.Column(db.Integer, ForeignKey('tweets.id'))
+    text = db.Column(db.String(400))
+
+    user = db.relationship("Users")
+    tweet = db.relationship("Tweets")
+    
+    def __str__(self):
+        # return '<User %r>' % self.username
+        return f'{self.tweet.id} <{self.text}> @{self.user.username}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tweet_id": self.tweet.id,
+            "tweet": self.tweet.text,
+            "user": self.user.username,
+            "text": self.text
+            # do not serialize the password, its a security breach
+        }
+
+
+class Likes(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    tweet_id = db.Column(db.Integer, ForeignKey('tweets.id'))
+
+    user = db.relationship("Users")
+    tweet = db.relationship("Tweets")
+    
+    def __str__(self):
+        # return '<User %r>' % self.username
+        # return f'@{self.user.username}'
+        return 'Donde cojones sale esto????'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "text": self.text
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
