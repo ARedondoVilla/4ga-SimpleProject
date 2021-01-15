866ca7834bd1d83a1df2359f24041f5547a9b634
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-15 11:58:21 +0000

    Initial style Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="p-2 flex-grow-1 bd-highlight">
+				<Link to="/">Twitter</Link>
 			</div>
+			<div className="p-2 bd-highlight">Seguidores</div>
+			<div className="p-2 bd-highlight">Seguidos</div>
+			<div className="p-2 bd-highlight">Likes</div>
+			<div className="p-2 bd-highlight">Retweet</div>
 		</nav>
 	);
 };
