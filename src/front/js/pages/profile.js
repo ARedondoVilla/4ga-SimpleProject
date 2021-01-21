8e8afbe978c5d2dc25089740c62dccc59fcaa0d2
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-21 14:20:22 +0000

    layout profile's card

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,13 +1,28 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import { Card } from "../component/card";
 
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container text-center mt-5">
+		<div className="container mt-5">
 			<h1>Perfil de usuario</h1>
+			<div className="row no-gutters justify-content-center">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 		</div>
 	);
 };
